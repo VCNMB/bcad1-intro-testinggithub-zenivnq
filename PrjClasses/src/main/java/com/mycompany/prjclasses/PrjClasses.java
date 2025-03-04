a8464b6c20c03e2929ba4c86a06852eb9c6a0cff
zenivnq <zibavnq@gmail.com>
2025-03-04 11:39:40 +0200

    Updated PrjClasses
    Added netbeans

--- PrjClasses/src/main/java/com/mycompany/prjclasses/PrjClasses.java
+++ PrjClasses/src/main/java/com/mycompany/prjclasses/PrjClasses.java
@@ -11,6 +11,6 @@ package com.mycompany.prjclasses;
 public class PrjClasses {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hello World! I just did my first GitHub project and want to go home! ");
     }
 }
