94ad6b7c288f646690a7c1731f6e39c543721d8d
zenivnq <zibavnq@gmail.com>
2025-03-11 10:40:13 +0200

    Classes Activity

--- PrjClasses/src/main/java/com/mycompany/prjclasses/PrjClasses.java
+++ PrjClasses/src/main/java/com/mycompany/prjclasses/PrjClasses.java
@@ -11,6 +11,12 @@ package com.mycompany.prjclasses;
 public class PrjClasses {
 
     public static void main(String[] args) {
-        System.out.println("Hello World! I just did my first GitHub project and want to go home! ");
+        
+        newClass nc = new newClass(); // creating an object of newClass
+        
+        nc.helloClass(); // have to use variable name nc to communicate with the class
+        
+        System.out.println(nc.strSecondClassVar); // output variable from 2nd class
+        
     }
 }
---
+++ PrjClasses/src/main/java/com/mycompany/prjclasses/newClass.java
@@ -0,0 +1,23 @@
+package com.mycompany.prjclasses;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author lab_services_student
+ */
+public class newClass {
+    
+    public String strSecondClassVar = "Testing";
+
+     public void helloClass()
+     {
+          System.out.println("Hello from the 2nd class");
+          
+     }
+    
+
+}
